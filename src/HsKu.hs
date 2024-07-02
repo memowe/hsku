c59fe5666bc25a59a94ce91abc31d4839e9f5408
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2024-07-02 20:45:52 +0200

    Cleanup

--- src/HsKu.hs
+++ src/HsKu.hs
@@ -1,4 +1,12 @@
-module HsKu where
+module HsKu
+  (
+    -- * Data types
+      Language(..)
+    , Languages
+    , Haiku
+    -- * Haiku parsing
+    , parseHaiku
+  ) where
 
 import Data.Bool
 import Data.Char
@@ -12,6 +20,8 @@ import Text.Megaparsec.Char
 import Control.Applicative as A
 import Control.Monad.Reader
 
+-- Data types
+
 data Language = Language
   { name      :: Text
   , vowels    :: Set Char
@@ -20,60 +30,62 @@ data Language = Language
 
 type Languages = Map Text Language
 
-sylChars :: Language -> Set Char
-sylChars = S.union <$> vowels <*> S.map T.head . diphtongs
-
 type Haiku = (Text, Text, Text)
+
+-- Haiku parsing
 
 type Parser = ParsecT Void Text (Reader Language)
 
-pVowel :: Parser Char
-pVowel = do
-  vs <- asks (S.toList . vowels)
-  choice (char' <$> vs)
-
-pDiphtong :: Parser Text
-pDiphtong = do
-  dps <- asks (S.toList . diphtongs)
-  choice (string' <$> dps)
-
-pSyl :: Parser Text
-pSyl = try pDiphtong <|> (T.singleton <$> pVowel)
-
-pSep :: Parser Text
-pSep = do
-  syls <- asks sylChars
-  takeWhile1P Nothing (not . (`S.member` syls))
-
-pWordSep :: Parser Text
-pWordSep = pSep >>= guarded (T.any isSpace)
-
-pSylSep :: Parser (Text, Text)
-pSylSep = (,) <$> pSyl <*> pSep
-
-pSyls :: Int -> Parser Text
-pSyls n = do
-  sylSeps <- P.count (n-1) pSylSep
-  lastSyl <- pSyl
-  let initSyls = T.concat (uncurry append <$> sylSeps)
-  return $ initSyls <> lastSyl
-
-pHaiku :: Parser Haiku
-pHaiku = do
-  start1          <- option "" pSep
-  line1           <- pSyls 5
-  (end1, start2)  <- splitWordSep <$> pWordSep
-  line2           <- pSyls 7
-  (end2, start3)  <- splitWordSep <$> pWordSep
-  line3           <- pSyls 5
-  end3            <- option "" pSep
-  return  ( start1 <> line1 <> end1
-          , start2 <> line2 <> end2
-          , start3 <> line3 <> end3
-          )
-
 parseHaiku :: Language -> Text -> Maybe Haiku
 parseHaiku lang = eitherToMaybe . flip runReader lang . runParserT pHaiku ""
+
+  where
+
+        pVowel :: Parser Char
+        pVowel = do
+          vs <- asks (S.toList . vowels)
+          choice (char' <$> vs)
+
+        pDiphtong :: Parser Text
+        pDiphtong = do
+          dps <- asks (S.toList . diphtongs)
+          choice (string' <$> dps)
+
+        pSyl :: Parser Text
+        pSyl = try pDiphtong <|> (T.singleton <$> pVowel)
+
+        pSep :: Parser Text
+        pSep = do
+          syls <- asks sylChars
+          takeWhile1P Nothing (not . (`S.member` syls))
+          where sylChars = S.union <$> vowels <*> S.map T.head . diphtongs
+
+        pWordSep :: Parser Text
+        pWordSep = pSep >>= guarded (T.any isSpace)
+
+        pSylSep :: Parser (Text, Text)
+        pSylSep = (,) <$> pSyl <*> pSep
+
+        pSyls :: Int -> Parser Text
+        pSyls n = do
+          sylSeps <- P.count (n-1) pSylSep
+          lastSyl <- pSyl
+          let initSyls = T.concat (uncurry append <$> sylSeps)
+          return $ initSyls <> lastSyl
+
+        pHaiku :: Parser Haiku
+        pHaiku = do
+          start1          <- option "" pSep
+          line1           <- pSyls 5
+          (end1, start2)  <- splitWordSep <$> pWordSep
+          line2           <- pSyls 7
+          (end2, start3)  <- splitWordSep <$> pWordSep
+          line3           <- pSyls 5
+          end3            <- option "" pSep
+          return  ( start1 <> line1 <> end1
+                  , start2 <> line2 <> end2
+                  , start3 <> line3 <> end3
+                  )
 
 -- Utility functions
 
@@ -84,6 +96,5 @@ splitWordSep sep =
       left  = T.dropWhileEnd isSpace rest
   in  (left, right)
 
--- soon to be in Control.Monad.Extra
 guarded :: Alternative f => (a -> Bool) -> a -> f a
-guarded = liftA2 (bool A.empty) pure
+guarded = liftA2 (bool A.empty) pure -- soon to be in Control.Monad.Extra
